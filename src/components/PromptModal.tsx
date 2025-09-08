ae530b05eb9b70e7f6a2f1aeddb48264e97d0562
Akash Mohan <akhikm4@gmail.com>
2025-09-08 02:05:08 -0500

    Prop + Data interfaces created + created basic skeleton

--- src/components/PromptModal.tsx
+++ src/components/PromptModal.tsx
@@ -1,1 +1,36 @@
 // Modal to allow the user to input prompts to OpenAI about their highlighted text.
+import { useEffect, useState } from "react";
+
+interface SubmitData {
+  selectedText: string;
+  prompt: string;
+}
+
+export interface PromptModalProps {
+  open: boolean;
+  selectedText: string;
+  onSubmit: (data: SubmitData) => void;
+  onClose: () => void;
+}
+
+// todo: write onSubmit to send prompt data to backend
+export default function PromptModal(props: PromptModalProps) {
+  const { open, selectedText, onSubmit, onClose } = props;
+  const [prompt, setPrompt] = useState("");
+
+  const handleSubmit = () => {
+    const submission: SubmitData = {
+      selectedText: selectedText,
+      prompt: prompt,
+    };
+
+    onSubmit(submission);
+    setPrompt("");
+  };
+
+  if (!open) {
+    return null;
+  }
+
+  return <div>PromptModal UI goes here.</div>;
+}
